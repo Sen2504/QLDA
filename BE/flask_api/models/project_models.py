2ca7ac02b03e9477591bb46b9fadca5b079f7d8b
DoNguyenMinhSang <144638950+Sen2504@users.noreply.github.com>
2025-09-26 14:33:03 +0700

    Thay doi model lien quan cho phuong thuc xoa cascade cua bang project, pj role, team. Chinh sua pj schemas

--- BE/flask_api/models/project_models.py
+++ BE/flask_api/models/project_models.py
@@ -7,10 +7,10 @@ class Project(db.Model):
     name = db.Column("NAME_PROJECT", db.String(200), nullable=False)
     description = db.Column("DESCRIPTION", db.Text, nullable=False)
 
-    sprints = db.relationship("Sprint", back_populates="project")
-    issues = db.relationship("Issue", back_populates="project")
-    user_stories = db.relationship("UserStory", back_populates="project")
-    project_roles = db.relationship("ProjectRole", back_populates="project")
+    sprints = db.relationship("Sprint", back_populates="project", cascade="all, delete-orphan")
+    issues = db.relationship("Issue", back_populates="project", cascade="all, delete-orphan")
+    user_stories = db.relationship("UserStory", back_populates="project", cascade="all, delete-orphan")
+    project_roles = db.relationship("ProjectRole", back_populates="project", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"<Project id={self.id} name={self.name}>"
