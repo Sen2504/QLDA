76ddc043efd49253dc653a6b32fbb3a134f8fe16
trancazang <huynhhuyentran2003@gmail.com>
2025-09-29 10:04:15 +0700

    cai thien toc do va them status project

--- BE/flask_api/utils/mail.py
+++ BE/flask_api/utils/mail.py
@@ -1,13 +1,17 @@
 # BE/flask_api/utils/email.py
+import threading
 from flask_mail import Message
 from flask import current_app
 from flask_api.extensions import mail
 
-def send_email(subject: str, recipients: list[str], html: str):
-    msg = Message(
-        subject=subject,
-        recipients=recipients,
-        sender=current_app.config.get("MAIL_DEFAULT_SENDER")  
-    )
-    msg.html = html
-    mail.send(msg)
+def _send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, recipients, html):
+    app = current_app._get_current_object()
+    msg = Message(subject, recipients=recipients, html=html)
+    thr = threading.Thread(target=_send_async_email, args=(app, msg))
+    thr.start()
+    return thr
+
